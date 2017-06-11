c3371986583ee5b5811c33ba1bffb4eeca9a5a32
Ky Hong Le <kyhoolee@gmail.com>
2017-06-11 10:52:35 +0700

    update

--- src/main/java/com/shipper/logic/notification/IosPush.java
+++ src/main/java/com/shipper/logic/notification/IosPush.java
@@ -1,9 +1,6 @@
 package com.shipper.logic.notification;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import com.notnoop.apns.APNS;
 import com.notnoop.apns.ApnsService;
@@ -13,17 +10,17 @@ public class IosPush {
 	public static boolean dev_or_product = true;
 	
 	
-	public static String dev_cert_shop = "Certificates_Shop_dev.p12";
+	public static String dev_cert_shop = "Shop_Dev_Certificates.p12";
 	public static String dev_pass_shop = "123456789";
 	
-	public static String pro_cert_shop = "Certificates_Shop_dev.p12";
+	public static String pro_cert_shop = "Shop_Dev_Certificates.p12";
 	public static String pro_pass_shop = "123456789";
 	
 	
-	public static String dev_cert_ship = "shipper_1_new_certificates.p12";//"ship_new_certificates.p12"; //"Shipper_dev_Certificates.p12";//
+	public static String dev_cert_ship = "Shipper_Dev_Certificates.p12";//"ship_new_certificates.p12"; //"Shipper_dev_Certificates.p12";//
 	public static String dev_pass_ship = "123456789";
 	
-	public static String pro_cert_ship = "shipper_1_new_certificates.p12";//"ship_new_certificates.p12";
+	public static String pro_cert_ship = "Shipper_Dev_Certificates.p12";//"ship_new_certificates.p12";
 	public static String pro_pass_ship = "123456789";
 	
 	
@@ -33,9 +30,11 @@ public class IosPush {
 	
 	public static void samplePush() {
 		List<String> tempRegis = new ArrayList<String>();
-		tempRegis.add("1");
-		tempRegis.add("5e9658a80174a038ad408e60bcff60ad1e618089bda5fd09c9905e0c004b6b38");
-		sendPushList(initService(User.role_shipper), tempRegis, "title", "message");
+//		tempRegis.add("1");
+//		tempRegis.add("651ca8f60c54bd1fdc2ab5956ef4318816272649bf51e9f7ab094a9bcf1c859b");
+//		sendPushList(initService(User.role_shipper), tempRegis, "title", "message");
+		sendPushList(initService(User.role_shop), Arrays.asList("c11e033a6a7e7fd603a1684f4dabd6643ecc06715c5e98c6a63dd7c691d32989"), "title", "message");
+		sendPushList(initService(User.role_shipper), Arrays.asList("651ca8f60c54bd1fdc2ab5956ef4318816272649bf51e9f7ab094a9bcf1c859b"), "title", "message");
 	}
 	
 	
@@ -61,7 +60,7 @@ public class IosPush {
 				+ "\"badge\":0,\"sound\":\"default\"}, "
 				+ "\"message\":"+message+""
 				+ "}";
-		
+		System.out.println(result);
 		return result;
 	}
 	
@@ -90,7 +89,7 @@ public class IosPush {
 				
 				ApnsService service = APNS
 						.newService()
-						.withCert(Thread.currentThread().getContextClassLoader().getResourceAsStream(dev_cert_shop), dev_pass_shop)
+						.withCert(IosPush.class.getClassLoader().getResourceAsStream(dev_cert_shop), dev_pass_shop)
 						//.withCert("Certificates_Shop_dev.p12","123456789")
 						//.withProductionDestination()
 						.withSandboxDestination().build();
@@ -99,10 +98,10 @@ public class IosPush {
 			} else {
 				ApnsService service = APNS
 						.newService()
-						.withCert(Thread.currentThread().getContextClassLoader().getResourceAsStream(pro_cert_shop), pro_pass_shop)
+						.withCert(IosPush.class.getClassLoader().getResourceAsStream(pro_cert_shop), pro_pass_shop)
 						//.withCert("doc/Certificates_Shop_dev.p12","123456789")
-						.withProductionDestination()
-						//.withSandboxDestination()
+//						.withProductionDestination()
+						.withSandboxDestination()
 						.build();
 				
 				return service;
@@ -113,7 +112,7 @@ public class IosPush {
 				System.out.println("init dev + role_ship");
 				ApnsService service = APNS
 						.newService()
-						.withCert(Thread.currentThread().getContextClassLoader().getResourceAsStream(dev_cert_ship), dev_pass_ship)
+						.withCert(IosPush.class.getClassLoader().getResourceAsStream(dev_cert_ship), dev_pass_ship)
 						//.withCert("Certificates_Shop_dev.p12","123456789")
 						//.withProductionDestination()
 						.withSandboxDestination().build();
@@ -122,10 +121,10 @@ public class IosPush {
 			} else {
 				ApnsService service = APNS
 						.newService()
-						.withCert(Thread.currentThread().getContextClassLoader().getResourceAsStream(pro_cert_ship), pro_pass_ship)
+						.withCert(IosPush.class.getClassLoader().getResourceAsStream(pro_cert_ship), pro_pass_ship)
 						//.withCert("doc/Certificates_Shop_dev.p12","123456789")
-						.withProductionDestination()
-						//.withSandboxDestination()
+//						.withProductionDestination()
+						.withSandboxDestination()
 						.build();
 				
 				return service;
@@ -139,10 +138,13 @@ public class IosPush {
 	
 	public static List<String> sendPushList(ApnsService apnsService, List<String> tempRegis,String title, String message){
 		List<String> returnVl = new ArrayList<String>();
-		apnsService.push(tempRegis, 
-				"{\"aps\":{\"alert\":\""+title+"\","
-				+"\"message\":"+message+","
-				+ "\"badge\":0,\"sound\":\"default\"}}");
+		apnsService.start();
+		String body = "{\"aps\":{\"alert\":\""+title+"\","
+				+"\"message\":\""+message+"\","
+				+ "\"badge\":0,\"sound\":\"default\"}}";
+		System.out.println(body);
+		apnsService.push(tempRegis,
+				body);
 		Map<String, Date> listInActive = apnsService.getInactiveDevices();
 		for(String str : listInActive.keySet()){
 			returnVl.add(str.toLowerCase());	
